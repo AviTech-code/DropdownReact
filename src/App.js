8b2eaf7b90674714e1fc891832a2273154c35a30
Avishek Jaiswal <65487639+AviTech-code@users.noreply.github.com>
2023-09-14 01:50:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Dropdown from './components/Dropdown';
-
+//Storing data as items
 function App() {
   const items = ["Yes", 'Probably Not'];
   return (
     <div className="App">
+  {/* passing data as props in Dropdown components */}
       <Dropdown items={items} />
     </div >
   );
